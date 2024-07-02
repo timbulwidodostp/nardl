6848a3acb8c32645d81d3e75454766b3bd279ed8
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2024-07-02 22:40:39 +0700

    Use nardl With R Software

--- nardl/nardl.R
+++ nardl/nardl.R
@@ -3,7 +3,9 @@
 # IG : @olahdatasemarang_
 # (Nonlinear ARDL (Autoregressive Distributed Lag Model) NARDL) regression Use nardl With R Software
 install.packages("nardl")
+install.packages("quadprog")
 library("nardl")
+library("quadprog")
 nardl = read.csv("https://raw.githubusercontent.com/timbulwidodostp/nardl/main/nardl/nardl.csv",sep = ";")
 # Estimation (Nonlinear ARDL (Autoregressive Distributed Lag Model) NARDL) regression Use nardl With R Software
 nardl<-nardl(food~inf,nardl,ic="aic",maxlag = 4,graph = FALSE,case=3)
